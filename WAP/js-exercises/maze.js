719a4bcbfb761ce2fafc47466c9c94906b9c19ae
Rajeev Thapaliya <rajeevthapaliya@gmail.com>
2021-03-12 11:23:18 -0600

    Mouse Maze Lab

--- WAP/js-exercises/maze.js
+++ WAP/js-exercises/maze.js
@@ -1,2 +1,28 @@
 $( document ).ready(function() {
+  var gameStart = false;
+  $("#maze .boundary").mouseover(function(){
+    if(gameStart){
+      $("#maze .boundary").addClass("youlose");
+      $("#status").text("You lose!");
+    }
   });
+
+  $("#maze").mouseleave(function(){
+    if(gameStart){
+      $("#maze .boundary").addClass("youlose");
+      $("#status").text("You lose.");
+    }
+  });
+
+  $("#start").click(function(){
+    gameStart=true;
+    $(".boundary").removeClass("youlose");
+    $("#status").text("Game On!");
+  });
+
+  $("#end").mouseover(function(){ 
+    gameStart=false;   
+    $("#status").text("You win!");
+  });
+
+});
